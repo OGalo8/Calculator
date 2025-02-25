add9554d87002d7242a2a8626dffd4e2806359d2
ogalo8 <arielgalo39481@gmail.com>
2025-02-25 13:55:32 -0600

    Basic functionality completed. Check README.md

--- README.md
+++ README.md
@@ -9,3 +9,18 @@ The HTML/CSS mockup has been completed. Will need more styling, but that will be
 <!--  -->
 
 Added logic to show on display numbers, erase screen and change signs.
+
+By multiplying by -1 when changing signs, JS is converting the entered string to a number!!!
+
+<!--  -->
+
+Basic functionality is now complete.
+
+Will now add logic to:
+
+- Prevent using more than one decimal point per digit.
+- Prevent using more than one operation per calculation (limiting the lenght of the array so it executes the calculations when lenght == 3 might help)
+- Add functionality to the % button
+- Toggle button color (maybe there is already an option for this. All colors should reset with reboot function)
+- Show only 2 decimal places per result.
+- Limit the maximum number of digits available OR reduce the size of the font in the display
--- script.js
+++ script.js
@@ -1,44 +1,122 @@
-function addGlobalEventListener(type, selector, callback, parent = document) {
-  parent.addEventListener(type, (e) => {
-    if (e.target.matches(selector)) {
-      callback(e);
-    }
-  });
-}
+let firstDigit = 0;
+let secondDigit = 0;
+let operation;
+let toCalculate = [];
 
-const acBtn = document.querySelector(".ac-button");
-let AC = acBtn.innerHTML;
+let calculationDone = false;
 
-const sevenBtn = document.querySelector("#seven-button");
-let seven = sevenBtn.innerHTML;
+let operationRegex = /[x\+\-%/=]/;
+let secondDigitRegex = /[x\+\-%/=]\d+/;
+
+const myButtons = document.querySelector(".buttons-container");
 
 const display = document.querySelector("#screen-output");
 
 const allBtns = document.querySelectorAll("button");
-const allSelectors = {};
 
 allBtns.forEach((button) => {
-  button.className != "sign-button"
-    ? (button.value = button.innerHTML)
-    : (button.value = -1);
-  allSelectors[button.id] = document.querySelector(`#${button.id}`);
+  switch (button.className) {
+    case "sign-button":
+      button.value = -1;
+      break;
+    default:
+      button.value = button.innerHTML;
+      break;
+  }
 });
 
-addGlobalEventListener("click", "button", showOnDisplay);
-
 function showOnDisplay(e) {
-  let pressedBtn = e.target.value;
+  if (display.innerHTML == 0) display.innerHTML = "";
+
+  let currentSelector = e.target;
+  let pressedBtnValue = e.target.value;
   let pressedBtnClass = e.target.className;
 
   switch (pressedBtnClass) {
     case "digit-button":
-      display.innerHTML += pressedBtn;
+      if (calculationDone == true) resetVariables();
+      console.log(pressedBtnValue, firstDigit, toCalculate, calculationDone); //remove later
+      display.innerHTML += pressedBtnValue;
       break;
     case "ac-button":
-      display.innerHTML = "";
+      resetVariables();
       break;
     case "sign-button":
-      display.innerHTML *= pressedBtn;
+      display.innerHTML *= pressedBtnValue;
+      break;
+    case "operation-button":
+      firstDigit = display.innerHTML;
+      toCalculate.push(firstDigit);
+      operation = pressedBtnValue.match(operationRegex)[0];
+      toCalculate.push(operation);
+
+      display.innerHTML += operation;
+
+      console.log(firstDigit, operation, toCalculate, calculationDone); //remove later
+
+      currentSelector.style.backgroundColor = "red";
+
+      break;
+    case "equal-button":
+      let almostSecondDigit = display.innerHTML.match(secondDigitRegex)[0];
+      secondDigit = Number(almostSecondDigit.slice(1));
+      toCalculate.push(secondDigit);
+
+      currentSelector.style.backgroundColor = "green";
+
+      let result = operate();
+      display.innerHTML = result;
+
+      console.log(result);
+      calculationDone = true;
+
+      startCalculating();
+      console.log(pressedBtnValue, toCalculate, calculationDone);
+
       break;
   }
 }
+
+function startCalculating() {
+  myButtons.removeEventListener("click", handleButtonClick); // Removes event listener (in case it already exists)
+
+  myButtons.addEventListener("click", handleButtonClick); // Adds a new event listener
+}
+
+function resetVariables() {
+  firstDigit = 0;
+  secondDigit = 0;
+  toCalculate = [];
+  display.innerHTML = "";
+  calculationDone = false;
+}
+
+function handleButtonClick(e) {
+  if (e.target.matches("button")) {
+    showOnDisplay(e);
+  }
+}
+
+function operate() {
+  let currentResult;
+  switch (operation) {
+    case "%":
+      currentResult = toCalculate[0] / 100;
+      break;
+    case "/":
+      currentResult = toCalculate[0] / toCalculate[2];
+      break;
+    case "x":
+      currentResult = toCalculate[0] * toCalculate[2];
+      break;
+    case "-":
+      currentResult = toCalculate[0] - toCalculate[2];
+      break;
+    case "+":
+      currentResult = toCalculate[0] - toCalculate[2];
+      break;
+  }
+  return currentResult;
+}
+
+startCalculating();
